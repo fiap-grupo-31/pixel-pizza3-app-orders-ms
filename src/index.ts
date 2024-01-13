4548aab718bba2e5a195532af1d3b4e94ab347fd
Anderson Silva <anderson@andersonalves.com.br>
2024-01-13 15:33:14 -0300

    Ajuste values

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ dotenv.config({
   path: '.env'
 })
 
+console.log('API API_ORDER_BASEURL', process.env.API_ORDER_BASEURL)
+console.log('API API_PAYMENT_BASEURL', process.env.API_PAYMENT_BASEURL)
+console.log('API API_PRODUCTION_BASEURL', process.env.API_PRODUCTION_BASEURL)
 const dbconnection = new MongodbConnection(
 );
 const fastfoodApp = new FastfoodApp(dbconnection);
