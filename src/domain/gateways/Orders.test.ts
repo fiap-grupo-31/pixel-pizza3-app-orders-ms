15928c00c0e7507560a104fa4b2d383475856960
Andraad3 <joao.andraade25@gmail.com>
2024-01-17 08:13:04 -0300

    gateway order test 80%

--- src/domain/gateways/Orders.test.ts
+++ src/domain/gateways/Orders.test.ts
@@ -93,4 +93,81 @@ describe('OrdersGateway', () => {
     expect(result).toBe(true);
     expect(dbConnectionMock.remove).toHaveBeenCalledWith('orders', '1');
   });
+
+  it('deve retornar false ao tentar remover um pedido inexistente', async () => {
+    dbConnectionMock.remove.mockResolvedValueOnce(false);
+
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.remove('1');
+
+    expect(result).toBe(false);
+    expect(dbConnectionMock.remove).toHaveBeenCalledWith('orders', '1');
+  });
+
+  it('deve retornar false ao tentar atualizar um pedido inexistente', async () => {
+    dbConnectionMock.update.mockResolvedValueOnce(false);
+
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.update('1', '1', 1, 100, 'RECEIVE', 'APPROVED', 'Teste');
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar null quando o resultado for null', async () => {
+    dbConnectionMock.find.mockResolvedValueOnce(null);
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.find({});
+
+    expect(result).toBeNull();
+    expect(dbConnectionMock.find).toHaveBeenCalledWith('orders', {});
+  });
+
+  it('deve retornar um array de Orders quando o resultado não for null', async () => {
+    const mockData = [
+      {
+        id: '1',
+        protocol: '1',
+        customerId: '1',
+        quantity: 1,
+        amount: 100,
+        status: 'RECEIVE',
+        payment: 'APPROVED',
+        orderDescription: 'Teste',
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ];
+    dbConnectionMock.find.mockResolvedValueOnce(mockData);
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.find({});
+
+    expect(result).toBeInstanceOf(Array);
+    expect(result).toHaveLength(1);
+    if (result !== null) {
+      expect(result[0]).toBeInstanceOf(Orders);
+    }
+    expect(dbConnectionMock.find).toHaveBeenCalledWith('orders', {});
+  });
+
+  it('deve retornar true quando o ID é válido', async () => {
+    dbConnectionMock.isValidId.mockResolvedValue(true);
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.isValidId('1');
+
+    expect(result).toBe(true);
+  });
+
+  it('deve retornar false quando o ID não é válido', async () => {
+    dbConnectionMock.isValidId.mockResolvedValue(false);
+    const ordersGateway = new OrdersGateway(dbConnectionMock);
+
+    const result = await ordersGateway.isValidId('invalid-id');
+
+    expect(result).toBe(false);
+  });
 });
